e74de16497f8bd063efabad2a3c7080335760ea5
paranjay <paranjay.pj@gmail.com>
2015-12-12 04:15:06 -0500

    updating gitignore and removing .idea files and adding a Constant file

---
+++ app/src/main/java/com/ceti_sb/android/Constants.java
@@ -0,0 +1,17 @@
+package com.ceti_sb.android;
+
+/**
+ * Created by paranjay on 12/12/15.
+ */
+public class Constants {
+
+    public static String LoginPreferencesString = "loginPrefs";
+    public static String SaveLoginString = "saveLogin";
+    public static String ProfileString = "profile";
+
+    public static String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
+    public static String REGISTRATION_COMPLETE = "registrationComplete";
+
+
+
+}

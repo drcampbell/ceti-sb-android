88db575302f2172234cfb281c00894f8ae1486d4
jith <jith87@gmail.com>
2017-04-26 19:13:27 +0530

    search event,accept claim,registration

---
+++ app/src/androidTest/java/com/ceti_sb/android/registration/AcceptClaim.java
@@ -0,0 +1,108 @@
+package com.ceti_sb.android.registration;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import com.ceti_sb.android.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class AcceptClaim {
+
+    @Rule
+    public ActivityTestRule<LoginActivity> mActivityTestRule = new ActivityTestRule<>(LoginActivity.class);
+
+    @Test
+    public void acceptClaim() {
+        ViewInteraction editText = onView(
+                allOf(withId(R.id.email),
+                        withParent(allOf(withId(R.id.email_login_form),
+                                withParent(withId(R.id.login_form))))));
+        editText.perform(scrollTo(), replaceText("jith87@gmail.com"), closeSoftKeyboard());
+
+        ViewInteraction editText2 = onView(
+                allOf(withId(R.id.password),
+                        withParent(allOf(withId(R.id.email_login_form),
+                                withParent(withId(R.id.login_form))))));
+        editText2.perform(scrollTo(), replaceText("ontojith"), closeSoftKeyboard());
+
+        ViewInteraction button = onView(
+                allOf(withId(R.id.sign_in_button), withText("Sign in"),
+                        withParent(allOf(withId(R.id.email_login_form),
+                                withParent(withId(R.id.login_form))))));
+        button.perform(scrollTo(), click());
+
+        ViewInteraction twoLineListItem = onView(
+                allOf(childAtPosition(
+                        withId(android.R.id.list),
+                        1),
+                        isDisplayed()));
+        twoLineListItem.perform(click());
+
+        ViewInteraction twoLineListItem2 = onView(
+                allOf(childAtPosition(
+                        withId(android.R.id.list),
+                        0),
+                        isDisplayed()));
+        twoLineListItem2.perform(click());
+
+        ViewInteraction button2 = onView(
+                allOf(withId(R.id.accept_button), withText("Accept"), isDisplayed()));
+        button2.perform(click());
+
+        ViewInteraction relativeLayout = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.container_fragment_event),
+                                childAtPosition(
+                                        withId(R.id.fragment_container),
+                                        0)),
+                        0),
+                        isDisplayed()));
+        relativeLayout.check(matches(isDisplayed()));
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
